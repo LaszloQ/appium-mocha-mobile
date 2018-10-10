6139d670b726d43064f7009f436812c365e0d361
Laszlo Kiss <kiss_lszlo@ymail.com>
2018-10-10 10:28:36 +0300

    Android tests skeleton

--- tests/android/native-android.js
+++ tests/android/native-android.js
@@ -4,65 +4,37 @@ const config = require( "../../helpers/desiredCapabilities-android" ).options;
 const client = webdriverio.remote( config );
 
 
-before(function() {
-
-    this.timeout(50000);
-
-    return client.init();
-
-  });
-
-  after(function() {
-
-      return client.end();
-
+  before( function( ) {
+    this.timeout( 50000 );
+    return client.init( );
   });
 
 
-describe("Simple cases", function() {
+  after( function( ) {
+    return client.end( );
+  });
 
 
+  describe( "Action bar tab", function( ) {
 
 
-        describe("test - app - addition", function() {
+    it( "can be added", function( ) {
 
-            it("add - 3 - 3", function() {
-              return client
-                .element("~IntegerA").setValue("3")
-                .element("~IntegerB").setValue("3")
-                .click("~ComputeSumButton")
-                .element("~Answer").getText()
-                  .then(function(text) {
-                    expect(text).to.equal("6");
-                  })
-            });
+    });
 
 
-            it.only( "check alert", function( ) {
-              return client
-                .click( "~show alert" )
+    it( "the last one can be removed", function( ) {
 
-                .alertText( )
-                  .then( function( text ){
-                    expect( text ).to.equal( "Cool title\nthis alert is so cool." )
-                  })
-                .alertAccept( ).then( function( ){
-                  console.log( "safjksahfhajfasfhajk" )
-                  try {
-           return client.alertAccept();
-          } catch (e) {
-           console.log( e )
-         }
-                })
+    });
 
 
+    it( "can be toggled", function( ) {
 
-            });
-        });
-
-
+    });
 
 
+    it( "can all be removed", function( ) {
 
-
-});
+    });
+    
+  });
