b8e6d1a06a2e0e4c857a9097a4989e98bcc78f4e
Laszlo Kiss <kiss_lszlo@ymail.com>
2018-10-10 10:23:34 +0300

    Restructured tests

---
+++ helpers/desiredCapabilities-android.js
@@ -0,0 +1,10 @@
+exports.options = {
+  desiredCapabilities: {
+    platformName: "Android",
+    platformVersion: "8.0",
+    app: "/Users/laszlokiss/Desktop/Apps/ApiDemos-debug.apk",
+    deviceName: "Nexus5"
+  },
+  host: "localhost",
+  port: 4723
+};
---
+++ tests/iOS/native.js
@@ -0,0 +1,68 @@
+const webdriverio = require( "webdriverio" );
+const expect = require( "chai" ).expect;
+const config = require( "../../helpers/desiredCapabilities" ).options;
+const client = webdriverio.remote( config );
+
+
+before(function() {
+
+    this.timeout(50000);
+
+    return client.init();
+
+  });
+
+  after(function() {
+
+      return client.end();
+
+  });
+
+
+describe("Simple cases", function() {
+
+
+
+
+        describe("test - app - addition", function() {
+
+            it("add - 3 - 3", function() {
+              return client
+                .element("~IntegerA").setValue("3")
+                .element("~IntegerB").setValue("3")
+                .click("~ComputeSumButton")
+                .element("~Answer").getText()
+                  .then(function(text) {
+                    expect(text).to.equal("6");
+                  })
+            });
+
+
+            it.only( "check alert", function( ) {
+              return client
+                .click( "~show alert" )
+
+                .alertText( )
+                  .then( function( text ){
+                    expect( text ).to.equal( "Cool title\nthis alert is so cool." )
+                  })
+                .alertAccept( ).then( function( ){
+                  console.log( "safjksahfhajfasfhajk" )
+                  try {
+           return client.alertAccept();
+          } catch (e) {
+           console.log( e )
+         }
+                })
+
+
+
+            });
+        });
+
+
+
+
+
+
+});
